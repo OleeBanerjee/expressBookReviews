ec5aaf72bb35366244403d296b06fe669d605f2e
OleeBanerjee <olee1banerjee@gmail.com>
2025-08-12 15:09:40 -0400

    up to task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,20 @@ const axios = require('axios');
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required" });
+    }
+    if (users.some(u => u.username === username)) {
+        return res.status(400).json({ message: "Username already exists" });
+    } else {
+
+        users.push({ username, password });
+        console.log(users);
+        return res.status(201).json({ message: "User registered successfully" });
+    }
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
@@ -92,7 +105,13 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (!book || !book.reviews) {
+        return res.status(404).json({ message: "Reviews not found for this book" });
+    }
+    return res.status(200).json(book.reviews);
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
